a113244dc28c19fa6b31382cf7156f746f37c250
Cameron Adamez <cameron@soycow.org>
2014-02-21 15:49:39 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Esphand
+-------
+
+Its smoke is used for medicinal purposes and to ward off evil eye.
---
+++ esphand.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+from datetime import date, timedelta
+
+app = Flask(__name__)
+
+@app.route("/")
+def dashboard():
+	return render_template('dashboard.html') 
+
+@app.route("/upcoming")
+def upcoming():
+	return render_template('upcoming.html') 
+
+@app.route("/new")
+def new_article():
+	return render_template('new_article.html')
+@app.route("/new", methods=['POST'])
+def other():
+	return str(request.form)
+
+@app.route("/edit")
+def edit_article():
+	if request.args.get('lock', '') == 'true':
+		template = 'locked.html'
+	else:
+		template = 'revisions.html'
+	return render_template(template)
+
+if __name__ == "__main__":
+	app.run(debug=True)
