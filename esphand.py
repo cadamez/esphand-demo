414764a84700289dbc85821d05465aedac8b4a87
Cameron Adamez <cameron@soycow.org>
2014-04-17 16:07:42 -0700

    Adding a fake login and slowly rebuilding the dashboard in the case of a writer.

--- esphand.py
+++ esphand.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for, escape
 from datetime import date, timedelta
 import os
 
@@ -6,7 +6,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def dashboard():
-	return render_template('dashboard.html') 
+    if 'username' in session:
+        return render_template('dashboard.html', username=escape(session['username'])) 
+    else:
+        return redirect(url_for('login'))
 
 @app.route("/upcoming")
 def upcoming():
@@ -31,5 +34,33 @@ def edit_article():
 def preview_article():
 	return render_template("preview.html")
 
+@app.route("/sections/<sectionname>/edit")
+def edit_section(sectionname):
+	return render_template("arrange_section.html")
+
+#@app.route('/')
+#def index():
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('dashboard'))
+    return '''
+        <form action="" method="post">
+            <p><label>Username:</label><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+# set the secret key.  keep this really secret:
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+
 if __name__ == "__main__":
-	app.run(debug=os.getenv('FLASK_DEBUG', False))
+	app.run(debug=bool(os.getenv('FLASK_DEBUG', False)))
